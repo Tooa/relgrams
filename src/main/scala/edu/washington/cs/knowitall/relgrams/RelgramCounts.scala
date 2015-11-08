Tooa/relgrams

commit 59f8def2b4c32c44b5fca80520e7910dfbc86ea9

added tuple documents indexer

src/main/scala/edu/washington/cs/knowitall/relgrams/RelgramsExtractor.scala
@@ -193,7 +193,7 @@ class RelgramsExtractor(maxWindow:Int, equality:Boolean, noequality:Boolean) {
 
       getRecordsIterator.filter(innerIndex => {
                            val inner = innerIndex._1
-                           val filterVal = areFromDifferentExtractions(outer, innerIndex._1)
+                           val filterVal = areFromDifferentExtractions(outer, inner)
                            filterVal
                         })
                         .filter(innerIndex => {
@@ -202,7 +202,7 @@ class RelgramsExtractor(maxWindow:Int, equality:Boolean, noequality:Boolean) {
                             filterVal
                         })
                         .filter(innerIndex => { val inner = innerIndex._1
-                            val filterVal = outer.sentid != innerIndex._1.sentid || !subsumes(outer, innerIndex._1)
+                            val filterVal = if(outer.sentid == inner.sentid) !subsumes(outer, inner) else false//outer.sentid != inner.sentid || !subsumes(outer, inner)
                             filterVal
                         })
                         .foreach(innerIndex => {

src/main/scala/edu/washington/cs/knowitall/relgrams/scoobi/RelgramsExtractorScoobiApp.scala
@@ -109,8 +109,6 @@ object RelgramsExtractorScoobiApp extends ScoobiApp{
     import TuplesDocumentWithCorefMentions._
     import RelgramCounts._
     import RelationTuple._
-    val counter = new RelgramsCounter(maxSize=5)
-
     val relgrams: DList[(Map[String, RelgramCounts], Map[String, RelationTupleCounts])] = tuplesDocuments.map(document => {
       val extractor = new RelgramsExtractor(maxWindow, equality, noequality)
       extractor.extractRelgramsFromDocument(document)

src/main/scala/edu/washington/cs/knowitall/relgrams/solr/IndexScoobiApp.scala
@@ -54,6 +54,7 @@ object IndexScoobiApp extends ScoobiApp {
       new ToSolrDocument(solrServer)
     }
 
+
     import Measures._
     val output:DList[Measures]  = TextInput.fromTextFile(inputPath).flatMap(line => Measures.fromSerializedString(line) match {
       case Some(measures:Measures) => {

src/main/scala/edu/washington/cs/knowitall/relgrams/solr/IndexTupleDocuments.scala
@@ -1,0 +1,81 @@
+package edu.washington.cs.knowitall.relgrams.solr
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: niranjan
+ * Date: 3/24/13
+ * Time: 8:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+import scala.collection.JavaConversions._
+import com.nicta.scoobi.application.ScoobiApp
+import scopt.mutable.OptionParser
+import edu.washington.cs.knowitall.relgrams.{TuplesDocument, TuplesDocumentWithCorefMentions}
+import com.nicta.scoobi.core.DList
+import com.nicta.scoobi.io.text.{TextOutput, TextInput}
+import org.apache.solr.client.solrj.impl.{XMLResponseParser, HttpSolrServer}
+import org.slf4j.LoggerFactory
+import org.apache.solr.common.SolrInputDocument
+import collection.mutable
+
+import com.nicta.scoobi.Persist._
+
+class ToSolrTuplesDocument(solrServer:HttpSolrServer) {
+  val logger = LoggerFactory.getLogger(this.getClass)
+  def addToIndex(id:Int, document:TuplesDocument) {
+    document.tupleRecords.foreach(record => {
+    val solrDoc = new SolrInputDocument
+    solrDoc.addField("id", document.docid + "-" + record.sentid + "-" + record.extrid)
+    solrDoc.addField("docid", document.docid)
+    solrDoc.addField("serialize", record.toString)
+    solrDoc.addField("sentence", record.sentence)
+    solrDoc.addField("arg1", record.arg1)
+    solrDoc.addField("arg1Head", record.arg1Head)
+    solrDoc.addField("rel", record.rel)
+    solrDoc.addField("relHead", record.relHead)
+    solrDoc.addField("arg2", record.arg2)
+    solrDoc.addField("arg2Head", record.arg2Head)
+    solrDoc.addField("arg1Types", record.arg1Types.mkString(","))
+    solrDoc.addField("arg2Types", record.arg2Types.mkString(","))
+    solrServer.add(solrDoc)
+    })
+  }
+
+}
+
+
+object IndexTupleDocuments extends ScoobiApp{
+
+  def run() {
+    var inputPath, solrURL, outputPath = ""
+    val parser = new OptionParser() {
+      arg("inputPath", "hdfs input path", {str => inputPath = str})
+      arg("solr", "solr server path", {str => solrURL = str})
+      arg("outputPath", "hdfs output path", { str => outputPath = str })
+    }
+    if (!parser.parse(args)) return
+    import TuplesDocumentWithCorefMentions._
+    println("This is run once....")
+    def loadTupleDocuments(inputPath: String):DList[TuplesDocumentWithCorefMentions] = {
+      TextInput.fromTextFile(inputPath)
+        .flatMap(x => TuplesDocumentWithCorefMentions.fromString(x))
+    }
+    val tosolrs = new mutable.HashMap[Thread, ToSolrTuplesDocument] with mutable.SynchronizedMap[Thread, ToSolrTuplesDocument]
+    def getToSolr(solrURL:String) = {
+      val solrServer = new HttpSolrServer(solrURL)
+      solrServer.setParser(new XMLResponseParser())
+      new ToSolrTuplesDocument(solrServer)
+    }
+
+    val tupleDocuments = loadTupleDocuments(inputPath)
+    val out:DList[String] = tupleDocuments.flatMap(td => {
+      val tosolr = tosolrs.getOrElseUpdate(Thread.currentThread(), getToSolr(solrURL))
+      tosolr.addToIndex(td.tuplesDocument.docid.hashCode, td.tuplesDocument)
+      None
+    })
+    persist(TextOutput.toTextFile(out, outputPath))
+
+  }
+}

src/main/scala/edu/washington/cs/knowitall/relgrams/solr/SolrSearchWrapper.scala
@@ -13,16 +13,37 @@ import org.apache.solr.client.solrj.{SolrQuery, SolrServer}
 import org.apache.solr.client.solrj.impl.{XMLResponseParser, HttpSolrServer}
 
 import scala.collection.JavaConversions._
-import edu.washington.cs.knowitall.relgrams.{AffinityMeasures, Measures, RelationTuple}
+import edu.washington.cs.knowitall.relgrams.{TypedTuplesRecord, AffinityMeasures, Measures, RelationTuple}
 
 import javax.xml.stream.XMLResolver
+import org.apache.solr.common.SolrDocument
 
-class SolrSearchWrapper(solrBaseUrl:String) {
+class SolrSearchWrapper(solrBaseUrl:String, solrDocUrl:String) {
 
   val logger = LoggerFactory.getLogger(this.getClass)
 
   val server = new HttpSolrServer(solrBaseUrl)
   server.setParser(new XMLResponseParser)
+
+  val docServer = new HttpSolrServer(solrDocUrl)
+  docServer.setParser(new XMLResponseParser)
+
+  def findTypedTuplesRecord(id:String):Option[TypedTuplesRecord] = {
+    val solrQuery = new SolrQuery
+    solrQuery.setQuery("*.*")
+    solrQuery.setRows(10)
+    solrQuery.addFilterQuery("id:" + id)
+    println("SolrQuery: " + solrQuery.toString)
+    val results = docServer.query(solrQuery)
+    results.getResults.find(x => x.getFieldValue("id").toString.equals(id)) match {
+      case Some(idresult:SolrDocument) => {
+        TypedTuplesRecord.fromString(idresult.getFieldValue("serialize").toString)
+      }
+      case _ => None
+    }
+
+  }
+
 
   def toSolrQuery(query:RelgramsQuery):Option[SolrQuery] = {
     val solrQuery = new SolrQuery()
@@ -80,10 +101,19 @@ object SolrSearchWrapperTest{
   val logger = LoggerFactory.getLogger(this.getClass)
   def main(args:Array[String]){
     val url = args(0)
-    val server = new SolrSearchWrapper(url)
-    val query = new RelgramsQuery(RelationTuple.fromArg1RelArg2("", "pay", ""),MeasureName.bigram, 10, 0.5, 0.5, "ARG1RELARG2", "conditional", "both")
+    val docurl = args(1)
+    val testid = args(2)
+    val server = new SolrSearchWrapper(url, docurl)
+
+    /**val query = new RelgramsQuery(RelationTuple.fromArg1RelArg2("", "pay", ""),MeasureName.bigram, 10, 0.5, 0.5, "ARG1RELARG2", "conditional", "both")
     logger.info("relgramsQuery: " + query.toHTMLString)
     val results = server.search(query)
     logger.info(results.mkString(","))
+     */
+    server.findTypedTuplesRecord(testid) match {
+      case Some(record:TypedTuplesRecord) => logger.info("Found record: " + record.toString)
+      case None => logger.info("Failed to find record for id: " + testid)
+    }
+
   }
 }

src/main/scala/edu/washington/cs/knowitall/relgrams/utils/MapUtils.scala
@@ -96,10 +96,11 @@ object MapUtils {
 
     var cmap = new mutable.HashMap[Int, Int]()
     updateCounts(cmap, 1, 0)
-    updateCounts(cmap, 10, 1)
-    updateCounts(cmap, 20, 1)
-    updateCounts(cmap, 30, 2)
-    distributeCounts(cmap, 50)
+    updateCounts(cmap, 1, 1)
+    updateCounts(cmap, 2, 1)
+    updateCounts(cmap, 3, 2)
+    println("Map:%s".format(toIntIntCountsString(cmap)))
+    distributeCounts(cmap, 5)
     println("Map:%s".format(toIntIntCountsString(cmap)))
   }
   def updateCounts[A, B](counts:scala.collection.mutable.Map[A, B], key:A, count:B)(implicit numeric: Numeric[B]){

src/main/scala/edu/washington/cs/knowitall/relgrams/web/RelgramsViewerFilter.scala
@@ -284,6 +284,8 @@ object HtmlHelper{
 
     loginForm += equalityCheckBoxes(query).toString//equalityOptions(query)
     loginForm += "<br/>"
+    loginForm += mesureIndexOptions(query)
+    loginForm += "<br/>"
     /**loginForm += viewOptions(query)
     loginForm += measureOptions(query)
     loginForm += mesureIndexOptions(query)
@@ -305,7 +307,7 @@ object HtmlHelper{
 object RelgramsViewerFilter extends unfiltered.filter.Plan {
   val logger = LoggerFactory.getLogger(this.getClass)
 
-  var solrManager = new SolrSearchWrapper("http://rv-n15.cs.washington.edu:10000/solr/relgrams")
+  var solrManager = new SolrSearchWrapper("http://rv-n15.cs.washington.edu:10000/solr/relgrams", "http://rv-n15.cs.washington.edu:10000/solr/mar24-tupledocuments")
 
   def wrapHtml(content:String) = "<html>" + content + "</html>"
 
@@ -317,14 +319,16 @@ object RelgramsViewerFilter extends unfiltered.filter.Plan {
   val tableTags = "<table class=\"financial\">\n%s\n</table>\n"
   //def headerRow(measure:MeasureName.MeasureName) = "<tr><td><b>First (F)</b></td><td><b>Second (S)</b></td><td><b>%s</b></td><td><b>%s</b></td><td><b>%s</b></td><td><b>%s</b></td><td><b>%s</b></td></tr>".format("Measure", "#(F,S)", "#(S,F)", "#(F,*)", "#(S,*)")
 
-  def headerRow(measure:MeasureName.MeasureName) = {
+  def headerRow(measure:MeasureName.MeasureName, k:Int) = {
+    val windowName = "#%s(F,S)+#%s(S,F)".format(k, k)
     val headElem = <thead>
       <tr>
       <th witdh="5%">First Arg1</th><th witdh="25%">First Rel</th><th witdh="5%">First Arg2</th>
       <th width="0.0%"/>
       <th witdh="5%">Second Arg1</th><th witdh="25%">Second Rel</th><th witdh="5%">Second Arg2</th>
       <th width="0.0%"/>
-      <th witdh="5%">P(S|F)</th><th width="5%">#(F,S)+#(S,F)</th><th width="5%">#(F,S)</th><th width="5%">#(S,F)</th><th width="5%">#F</th><th width="5%">#S</th>
+      <th width="5%">{windowName}</th>
+      <th width="5%">P(S|F)</th><th width="5%">#(F,S)+#(S,F)</th><th width="5%">#(F,S)</th><th width="5%">#(S,F)</th><th width="5%">#F</th><th width="5%">#S</th>
       </tr>
     </thead>
     headElem.toString
@@ -396,6 +400,7 @@ object RelgramsViewerFilter extends unfiltered.filter.Plan {
     val rgc = undirRGC.rgc
     val fscount = maxOrElse(undirRGC.rgc.counts.values, 0)
     val bitermCount = maxOrElse(undirRGC.bitermCounts.values, 0)
+    val bitermCountsAtK = undirRGC.bitermCounts.getOrElse(query.measureIndex, 0)
     val sfcount = bitermCount - fscount
     var farg1Counts: mutable.Map[String, Int] = rgc.argCounts.firstArg1Counts
     var farg2Counts: mutable.Map[String, Int] = rgc.argCounts.firstArg2Counts
@@ -426,12 +431,13 @@ object RelgramsViewerFilter extends unfiltered.filter.Plan {
     <tr class={evenString}>
       {relationWithRelArgs(rgc.relgram.first, tfarg1Counts, tfarg2Counts)}<td/>
       {relationWithRelArgs(rgc.relgram.second, tsarg1Counts, tsarg2Counts)}<td/>
-      <td style={valStyle} witdh="5%">{measureVal}</td>
-      <td style={valStyle} witdh="5%">{bitermCount}</td>
-      <td style={valStyle} witdh="5%">{fscount}</td>
-      <td style={valStyle} witdh="5%">{sfcount}</td>
-      <td style={valStyle} witdh="5%">{measures.firstCounts}</td>
-      <td style={valStyle} witdh="5%">{measures.secondCounts}</td>
+      <td style={valStyle} width="5%">{bitermCountsAtK}</td>
+      <td style={valStyle} width="5%">{measureVal}</td>
+      <td style={valStyle} width="5%">{bitermCount}</td>
+      <td style={valStyle} width="5%">{fscount}</td>
+      <td style={valStyle} width="5%">{sfcount}</td>
+      <td style={valStyle} width="5%">{measures.firstCounts}</td>
+      <td style={valStyle} width="5%">{measures.secondCounts}</td>
     </tr>
 
 
@@ -486,7 +492,7 @@ object RelgramsViewerFilter extends unfiltered.filter.Plan {
 
   def renderSearchResults(measureName:String, query:RelgramsQuery, results:(String, Seq[(Measures, AffinityMeasures)])) = {
     var even = false
-    cssSoft + cssFinancial + wrapResultsTableTags(headerRow(query.measure) + "\n<br/>\n<tbody>" +
+    cssSoft + cssFinancial + wrapResultsTableTags(headerRow(query.measure, query.measureIndex) + "\n<br/>\n<tbody>" +
                          results._2.map(ma => {
                            even = !even
                            toResultsRow(measureName, query, ma._1, ma._2, even)
@@ -583,12 +589,15 @@ object RelgramsViewerFilter extends unfiltered.filter.Plan {
   def main(args:Array[String]){
 
     //var port = 10000
+    var relgramsURL, docsURL = ""
     val parser = new OptionParser() {
-      arg("solrURL", "hdfs input path", {str => solrManager = new SolrSearchWrapper(str)})
+      arg("solrURL", "hdfs input path", {str => relgramsURL = str})
+      arg("solrDocURL", "hdfs input path", {str => docsURL = str})
       opt("port", "port to run on.", {str => port = str.toInt})
       opt("host", "port to run on.", {str => host = str})
       opt("minFreq", "minimum frequency threshold", {str => minFreq = str.toInt})
     }
+    solrManager =  new SolrSearchWrapper(relgramsURL, docsURL)
     if (!parser.parse(args)) return
     unfiltered.netty.Http(port).plan(intentVal).run()
   }

commit 36419d95c123dc9795ac9341c4aa3c9ed825b845

* Fix issue that was introduced in commit 59f8def2b4c32c44b5fca80520e7910dfbc86ea9. False should be true to take the relgram in case the sentenceId's are different. * Added sample files * Refactored some parts

src/main/resources/one_sent_sample
@@ -1,0 +1,1 @@
+0 _DOCID_SEP_ 0	0	Angela Merkel likes Vladimir.	0	010202	Angela_SPAN_0-6	likes_SPAN_14-19	Vladimir_SPAN_20-28	Angela_SPAN_0-6	like_SPAN_14-18	Vladimir_SPAN_20-28	PERSON	PERSON

src/main/resources/two_sent_sample
@@ -1,0 +1,1 @@
+0 _DOCID_SEP_ 0	0	Angela Merkel likes Vladimir.	0	010202	Angela_SPAN_0-6	likes_SPAN_14-19	Vladimir_SPAN_20-28	Angela_SPAN_0-6	like_SPAN_14-18	Vladimir_SPAN_20-28	PERSON	PERSON _RECORD_SEP_ 0	1	Angela Merkel hates Vladimir.	0	010202	Angela_SPAN_0-6	hates_SPAN_14-19	Vladimir_SPAN_20-28	Angela_SPAN_0-6	hate_SPAN_14-18	Vladimir_SPAN_20-28	PERSON	PERSON

src/main/scala/edu/washington/cs/knowitall/relgrams/RelgramCounts.scala
@@ -56,9 +56,9 @@ object RelationTuple{
       val arg1 = iterator.next
       val rel = iterator.next
       val arg2 = iterator.next
-      var hashes = iterator.next.split(ID_SEP).map(x => x.toInt).toSet
-      var sentences = iterator.next.split(SENT_SEP).toSet
-      var ids = iterator.next.split(ID_SEP).toSet
+      val hashes = iterator.next.split(ID_SEP).map(x => x.toInt).toSet
+      val sentences = iterator.next.split(SENT_SEP).toSet
+      val ids = iterator.next.split(ID_SEP).toSet
       var arg1HeadCounts = new mutable.HashMap[String, Int]()
       if (iterator.hasNext) arg1HeadCounts ++= MapUtils.StringIntMapfromSafeCountsString(iterator.next)
       var arg2HeadCounts = new mutable.HashMap[String, Int]()

src/main/scala/edu/washington/cs/knowitall/relgrams/RelgramsExtractor.scala
@@ -9,21 +9,18 @@ package edu.washington.cs.knowitall.relgrams
  */
 
 
-import scala.collection.JavaConversions._
-
-import collection.mutable.{ArrayBuffer, HashMap}
-import collection.mutable
-import io.Source
 import java.io.PrintWriter
-import org.slf4j.LoggerFactory
-import scala.Predef._
-import scala.Some
 
-
+import edu.washington.cs.knowitall.relgrams.utils.MapUtils
 import edu.washington.cs.knowitall.relgrams.utils.MapUtils._
-import utils.MapUtils
 import edu.washington.cs.knowitall.tool.coref.Mention
-import edu.washington.cs.knowitall.tool.postag.PostaggedToken
+import org.slf4j.LoggerFactory
+
+import scala.Predef._
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+import scala.collection.mutable.HashMap
+import scala.io.Source
 
 class RelgramsExtractor(maxWindow:Int, equality:Boolean, noequality:Boolean) {
 
@@ -103,8 +100,8 @@ class RelgramsExtractor(maxWindow:Int, equality:Boolean, noequality:Boolean) {
 
   def extractRelgramsFromDocument(document:TuplesDocumentWithCorefMentions): (Map[String, RelgramCounts], Map[String, RelationTupleCounts]) = {
 
-    var relgramCountsMap = new mutable.HashMap[String, RelgramCounts]()
-    var relationTuplesMap = new mutable.HashMap[String, RelationTuple]()
+    val relgramCountsMap = new mutable.HashMap[String, RelgramCounts]()
+    val relationTuplesMap = new mutable.HashMap[String, RelationTuple]()
 
     def assignIndex(records:Seq[TypedTuplesRecord]) = {
       var index = 0
@@ -119,25 +116,22 @@ class RelgramsExtractor(maxWindow:Int, equality:Boolean, noequality:Boolean) {
       })
     }
     val prepositions = Set[String] ("in", "on", "at", "over", "by", "after", "near", "of")
-    def hasPreposition(rel:String) = rel.split(" ").find(x => prepositions.contains(x))
+    def hasPreposition(rel:String): Option[String] = rel.split(" ").find(x => prepositions.contains(x))
     def hasInferredPrepRelation(record:TypedTuplesRecord) = hasPreposition(record.relHead) match {
-      case Some(x:String) => !record.sentence.split(" ").contains(x)
+      case Some(prep:String) => !record.sentence.split(" ").contains(prep)
       case None => false
     }
+
+
+
     val prunedRecords:Seq[(TypedTuplesRecord, Int)] = assignIndex(document.tuplesDocument
                                                                           .tupleRecords
                                                                           .filter(record => !hasInferredPrepRelation(record)))
 
-                                                              //.filter(record => notInferredPrepRelation(record))
-                                                              //.sortBy(r => (r.sentid, r.extrid))
-                                                              //.zipWithIndex
-    /**println("Docsize\t%d\t%d\t%d\t%d".format(document.tuplesDocument.tupleRecords.size,
-                                             prunedRecords.size,
-                                             prunedRecords.map(r => r._1.sentid).max,
-                                             prunedRecords.map(r => r._2).max))*/
     val mentions = document.mentions
-    val sentencesWithOffsets = if(equality){
-    val trimdocument = TuplesDocumentGenerator.trimDocument(document.tuplesDocument)
+    // Maps from sentenceId to the respective offset e.g sentenceId 0 -> (0,0)
+    val sentencesWithOffsets: mutable.Map[Int, Int] = if(equality){
+      val trimdocument = TuplesDocumentGenerator.trimDocument(document.tuplesDocument)
       TuplesDocumentGenerator.sentenceIdsWithOffsets(trimdocument)._2
     }else{
       mutable.Map[Int, Int]()
@@ -168,13 +162,15 @@ class RelgramsExtractor(maxWindow:Int, equality:Boolean, noequality:Boolean) {
     }
 
     val prunedMentions = pruneMentions(mentions)
-    var rtcountsMap = new mutable.HashMap[String, RelationTupleCounts]()
+    val rtcountsMap = new mutable.HashMap[String, RelationTupleCounts]()
     val recordRelationTuples = new mutable.HashMap[(Int, Int), mutable.HashSet[RelationTuple]]()
+
+
     getRecordsIterator.foreach(index => {
       val record = index._1
       val (arg1s, arg2s) = argRepresentations(record)
-      var sentences = new mutable.HashSet[String]()
-      var ids = new mutable.HashSet[String]()
+      val sentences = new mutable.HashSet[String]()
+      val ids = new mutable.HashSet[String]()
       val rel = record.relHead
       arg1s.foreach(arg1 => {
         arg2s.foreach(arg2 => {
@@ -182,71 +178,70 @@ class RelgramsExtractor(maxWindow:Int, equality:Boolean, noequality:Boolean) {
         })
       })
     })
-    def areFromDifferentExtractions(one:TypedTuplesRecord, other:TypedTuplesRecord) = one.sentid != other.sentid || one.extrid != other.extrid
-    getRecordsIterator.foreach(outerIndex => {
 
-      val outer = outerIndex._1
-      val oindex = outerIndex._2
-      val outerStartOffset = if(equality) startingOffset(outer) else 0
-      val outerRelationTuples = recordRelationTuples.get((outer.sentid, outer.extrid)).get
+    def areFromDifferentExtractions(one:TypedTuplesRecord, other:TypedTuplesRecord) = one.sentid != other.sentid || one.extrid != other.extrid
+
+
+    getRecordsIterator.foreach { case (outerTupledRecord, outerIndex) =>
+
+      val outerStartOffset = if(equality) startingOffset(outerTupledRecord) else 0
+      val outerRelationTuples: mutable.HashSet[RelationTuple] = recordRelationTuples.get((outerTupledRecord.sentid, outerTupledRecord.extrid)).get
       val addedSeconds = new mutable.HashSet[String]()
 
-      getRecordsIterator.filter(innerIndex => {
-                           val inner = innerIndex._1
-                           val filterVal = areFromDifferentExtractions(outer, inner)
-                           filterVal
-                        })
-                        .filter(innerIndex => {
-                            val filterVal = (innerIndex._2 > oindex) && (innerIndex._2 <= oindex+maxWindow)
-                            val inner = innerIndex._1
-                            filterVal
-                        })
-                        .filter(innerIndex => { val inner = innerIndex._1
-                            val filterVal = if(outer.sentid == inner.sentid) !subsumes(outer, inner) else false//outer.sentid != inner.sentid || !subsumes(outer, inner)
-                            filterVal
-                        })
-                        .foreach(innerIndex => {
-        val iindex = innerIndex._2
-        val countWindow = iindex-oindex
-        val inner = innerIndex._1
+      // Take only different pairs into account i.e. different sentenceId or different extriId
+      val differentPairs = getRecordsIterator.filter { case(innerTupledRecord, _) =>
+        areFromDifferentExtractions(outerTupledRecord, innerTupledRecord)
+      }
+      // Take only pairs within the given window size into account
+      val withinWindowSize = differentPairs.filter { case(_, innerIndex) =>
+          (innerIndex > outerIndex) && (innerIndex <= outerIndex + maxWindow)
+      }
+      // In case, the sentenceId is the same and just the exriId differs,
+      // proof that the actual relation tuple differs i.e. different head, rel, etc.
+      withinWindowSize.filter { case(innerTupledRecord, _) =>
+        if(outerTupledRecord.sentid == innerTupledRecord.sentid) !subsumes(outerTupledRecord, innerTupledRecord) else true //outer.sentid != inner.sentid || !subsumes(outer, inner)
+      }
+      .foreach{ case(innerTupledRecord, innerIndex) =>
+          val countWindow = innerIndex-outerIndex
 
-        val corefArgs:Option[(String, String, String, String)] = equality match {
-          case true => {
-            val innerStartOffset = startingOffset(inner)
-            CoreferringArguments.coreferringArgs(outer, outerStartOffset, inner, innerStartOffset, prunedMentions)
-          }
-          case false => None
-        }
-        val innerRelationTuples = recordRelationTuples.get((inner.sentid, inner.extrid)).get
-
-        import edu.washington.cs.knowitall.relgrams.utils.Crossable._
-        val firstSeconds = (outerRelationTuples x innerRelationTuples)
-          firstSeconds.foreach(firstsecond => {
-          val first = firstsecond._1
-          val second = firstsecond._2
-          noequality match {
+          val corefArgs:Option[(String, String, String, String)] = equality match {
             case true => {
-              val relKey = relgramKey(first, second)
-              if (!addedSeconds.contains(relKey)) {
-                //val sentWindow = (inner.sentid - outer.sentid)
-                //println("Adding relgrams with countWindow: " + countWindow + " and from sentences: " + inner.sentid + " and " + outer.sentid + " sent window: " + sentWindow)
-                addToRelgramCounts(outer, inner, first, second, countWindow, relgramCountsMap)
+              val innerStartOffset = startingOffset(innerTupledRecord)
+              CoreferringArguments.coreferringArgs(outerTupledRecord, outerStartOffset, innerTupledRecord, innerStartOffset, prunedMentions)
+            }
+            case false => None
+          }
+          val innerRelationTuples = recordRelationTuples.get((innerTupledRecord.sentid, innerTupledRecord.extrid)).get
+
+          import edu.washington.cs.knowitall.relgrams.utils.Crossable._
+          val firstSeconds = (outerRelationTuples x innerRelationTuples)
+            firstSeconds.foreach(firstsecond => {
+            val first = firstsecond._1
+            val second = firstsecond._2
+            noequality match {
+              case true => {
+                val relKey = relgramKey(first, second)
+                if (!addedSeconds.contains(relKey)) {
+                  val sentWindow = (innerTupledRecord.sentid - outerTupledRecord.sentid)
+                  //println("Adding relgrams with countWindow: " + countWindow + " and from sentences: " + innerTupledRecord.sentid + " and " + outerTupledRecord.sentid + " sent window: " + sentWindow)
+                  addToRelgramCounts(outerTupledRecord, innerTupledRecord, first, second, countWindow, relgramCountsMap)
+                }
+                addedSeconds += relKey
               }
-              addedSeconds += relKey
+              case false =>
             }
-            case false =>
-          }
 
-          corefArgs match {
-            case Some(resolvedArgs:(String, String, String, String)) => {
-              extractEqualityRelgrams(resolvedArgs, outer, inner, first, second, relationTuplesMap, relgramCountsMap, rtcountsMap, countWindow, addedSeconds)
+            corefArgs match {
+              case Some(resolvedArgs:(String, String, String, String)) => {
+                extractEqualityRelgrams(resolvedArgs, outerTupledRecord, innerTupledRecord, first, second, relationTuplesMap, relgramCountsMap, rtcountsMap, countWindow, addedSeconds)
+              }
+              case None =>
             }
-            case None =>
-          }
 
-        })
-      })
-    })
+          })
+        }
+    }
+
     val outputRelationCountsMap = new mutable.HashMap[String, RelationTupleCounts]()
     rtcountsMap.foreach(kv => {
       val splits = splitRTCKey(kv._1)

src/main/scala/edu/washington/cs/knowitall/relgrams/RelgramsLocalApp.scala
@@ -8,11 +8,9 @@ package edu.washington.cs.knowitall.relgrams
  * To change this template use File | Settings | File Templates.
  */
 import org.slf4j.LoggerFactory
-import scopt.mutable.OptionParser
 import io.Source
 import java.io.{PrintWriter, File}
 import scopt.mutable.OptionParser
-import scala.Some
 
 
 object RelgramsLocalApp {
@@ -22,8 +20,8 @@ object RelgramsLocalApp {
     var inputPath, outputPath = ""
     var maxWindow = 10
     var maxSize = 5
-    var equality = false
-    var noequality = false
+    var equality = true
+    var noequality = true
 
     val parser = new OptionParser() {
       arg("inputPath", "hdfs input path", {str => inputPath = str})

src/main/scala/edu/washington/cs/knowitall/relgrams/TuplesDocument.scala
@@ -32,7 +32,7 @@ object TuplesDocumentGenerator{
   }
 
   def sentenceIdsWithOffsets(document: TuplesDocument) = {//List[(String, Int)] = {
-  var offset = 0
+    var offset = 0
     var sentencesMap = new HashMap[Int, String]//new ArrayBuffer[String]()
     var sentidOffsetsMap = new mutable.HashMap[Int, Int]()
     document.tupleRecords.iterator.foreach(record => sentencesMap += record.sentid -> record.sentence)
@@ -40,7 +40,7 @@ object TuplesDocumentGenerator{
     var offsets = new ArrayBuffer[Int]()
     sentencesMap.keys.toSeq.sortBy(key => key).foreach(key => {//document.tupleRecords.iterator.foreach(record => {
     //println("key: " + key)
-    val sentence = sentencesMap(key)
+      val sentence = sentencesMap(key)
       sentences += sentence
       offsets += offset
       sentidOffsetsMap += key -> offset